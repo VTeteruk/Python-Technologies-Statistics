ecfd83fe276c06e51dd7159a90371e2c0202e6fe
Vlad Teteruk <vladteteruk01@gmail.com>
2023-08-14 21:58:47 +0300

    Add function to scrap data

--- parser/parser.py
+++ parser/parser.py
@@ -16,3 +16,16 @@ logging.basicConfig(
 def create_soup(url: str) -> BeautifulSoup:
     page = requests.get(url).content
     return BeautifulSoup(page, "html.parser")
+
+
+def parse_page_number(soup: BeautifulSoup):
+    number = int(soup.select("li.page-item > a.page-link")[-2].text)
+    return number if number else 0
+
+
+def parse_vacancies_references(soup: BeautifulSoup) -> list:
+    return [reference["href"] for reference in soup.select("a.profile")]
+
+
+def parse_detailed_vacancy(soup: BeautifulSoup):
+    return soup.select_one("div.mb-4")
