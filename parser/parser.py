7005760b06c9e3ec55a0eefb7432d9455c76db26
Vlad Teteruk <vladteteruk01@gmail.com>
2023-08-14 21:59:46 +0300

    Combine all parsing functions and write data to csv file

--- parser/parser.py
+++ parser/parser.py
@@ -29,3 +29,41 @@ def parse_vacancies_references(soup: BeautifulSoup) -> list:
 
 def parse_detailed_vacancy(soup: BeautifulSoup):
     return soup.select_one("div.mb-4")
+
+
+def calculate_technologies(soup: BeautifulSoup) -> None:
+    for technology in config.TECHNOLOGIES:
+        if re.findall(f" +{technology.lower()}", soup.get_text().lower()):
+            RESULTS[technology] += 1
+            continue
+
+
+def combine_data() -> None:
+    RESULTS["JavaScript"] += RESULTS["JS"]
+    del RESULTS["JS"]
+
+
+def write_to_csv(data: dict):
+    with open("../py-technologies.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
+        writer.writerow(data.values())
+
+
+def main(url: str) -> None:
+    for number in range(1, parse_page_number(create_soup(url)) + 1):
+        logging.info(f"Parsing page #{number}")
+        vacancies_references = parse_vacancies_references(create_soup(url + f"&page={number}"))
+        for reference in vacancies_references:
+            logging.info(reference)
+            parsed_soup = parse_detailed_vacancy(
+                create_soup(urljoin(config.DJINNI_HOME_URL, reference))
+            )
+            calculate_technologies(parsed_soup)
+
+    combine_data()
+    write_to_csv(RESULTS)
+
+
+if __name__ == "__main__":
+    main(config.DJINNI_PYTHON_JOBS_URL)
