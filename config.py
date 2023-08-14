85da892bbe83263a8b6871dc51f32e32220fb347
Vlad Teteruk <vladteteruk01@gmail.com>
2023-08-14 18:17:21 +0300

    Add config.py file with technologies that will be checked

---
+++ config.py
@@ -0,0 +1,15 @@
+TECHNOLOGIES = (
+    "Python",
+    "Django",
+    "SQL",
+    "REST",
+    "Math",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "API",
+    "Docker",
+    "OOP",
+    "PostgreSQL",
+    "Linux"
+)
