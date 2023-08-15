8915938a3f8a89bc41d98437e2454004f90e11a3
Vlad Teteruk <vladteteruk01@gmail.com>
2023-08-15 10:24:58 +0300

    Add another color if value > avarage value

--- analysis.py
+++ analysis.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 data = pd.read_csv("py-technologies.csv")
 df = pd.DataFrame(data).sort_values("Value", ascending=False)
 
+colors = ["red" if value > df["Value"].mean() else "blue" for value in df["Value"]]
+
 plt.figure(figsize=(10, 6))
-plt.bar(df["Technology"], df["Value"], color="blue", width=0.4)
+plt.bar(df["Technology"], df["Value"], color=colors, width=0.4)
 plt.xticks(rotation=90)
 plt.show()
