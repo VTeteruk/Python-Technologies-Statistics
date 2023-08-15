e0869dc6a6c1e0bd6ea48ae11a39caed22e38295
Vlad Teteruk <vladteteruk01@gmail.com>
2023-08-15 10:29:54 +0300

    Decorate plot

--- analysis.py
+++ analysis.py
@@ -6,7 +6,11 @@ df = pd.DataFrame(data).sort_values("Value", ascending=False)
 
 colors = ["red" if value > df["Value"].mean() else "blue" for value in df["Value"]]
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(12, 6))
 plt.bar(df["Technology"], df["Value"], color=colors, width=0.4)
 plt.xticks(rotation=90)
+plt.title("Python technologies according to djinni vacancies")
+plt.xlabel("Technologies")
+plt.ylabel("Value")
+plt.tight_layout()
 plt.show()
